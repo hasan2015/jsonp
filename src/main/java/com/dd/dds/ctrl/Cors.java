e0efa4d696075f3b37e2ee10020f3cbdca905c88
hasan2015 <13588068929@163.com>
2016-05-21 22:50:53 +0800

    response.addHeader("Access-Control-Allow-Origin", "*");ok
    拦截器没有调测完，按理应该没问题，暂时先搁置，下一步是“原生页面切换控件研究”

--- src/main/java/com/dd/dds/ctrl/Cors.java
+++ src/main/java/com/dd/dds/ctrl/Cors.java
@@ -4,9 +4,12 @@
 package com.dd.dds.ctrl;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.codehaus.jackson.map.util.JSONPObject;
 import org.springframework.stereotype.Controller;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.dd.dds.dto.Company;
+import com.dd.dds.dto.User;
 
 /**
  * @author Hasan
@@ -44,4 +48,28 @@ public class Cors {
 		company.setPhone("135880768929");
 		return new JSONPObject(callbackparam, company);
 	}
+
+	@ResponseBody
+	@RequestMapping(value = "/json", method = RequestMethod.GET)
+	public List<User> json(HttpServletResponse response) {
+		//response.addHeader("Access-Control-Allow-Origin", "*");//调测通过，可以跨域访问了
+		 
+		List<User> lUses = new ArrayList<User>();
+		User user = new User();
+		user.setAge("10");
+		user.setId("201");
+		user.setName("张三");
+		user.setSex("nv");
+		lUses.add(user);
+		user = new User();
+		user.setAge("12");
+		user.setId("203");
+		user.setName("李四");
+		user.setSex("nan");
+		lUses.add(user);
+
+		return lUses;
+
+	}
+
 }
---
+++ src/main/java/com/dd/dds/dto/User.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package com.dd.dds.dto;
+
+/**
+ * @author Hasan
+ * @Date 2016年5月21日 下午4:43:22
+ *
+ */
+public class User {
+	private String id;
+	private String name;
+	private String age;
+	private String sex;
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the age
+	 */
+	public String getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(String age) {
+		this.age = age;
+	}
+	/**
+	 * @return the sex
+	 */
+	public String getSex() {
+		return sex;
+	}
+	/**
+	 * @param sex the sex to set
+	 */
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+}
